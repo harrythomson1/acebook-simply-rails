9b7657b1186d11532632c6f06175c4743e0cfb4d
mitchell9-1 <mitchell.perry001@gmail.com>
2021-03-17 15:55:13 +0000

    Added like function and unable to like more than one post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,21 @@
 class LikesController < ApplicationController
   before_action :find_post
   def create
-    @post.likes.create(user_id: current_user.id)
-    redirect_to posts_url
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @post.likes.create(user_id: current_user.id)
+   end
+     redirect_to posts_url
   end
+
   private
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+  Like.where(user_id: current_user.id, post_id:
+  params[:post_id]).exists?
   end
 end
